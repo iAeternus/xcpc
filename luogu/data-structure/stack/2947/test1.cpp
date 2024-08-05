eceba857facf218a9041b76dab3a319a0dc0feb9
Ricky <w_ziwei2004@163.com>
2024-08-05 14:03:04 +0800

    2947

---
+++ luogu/data-structure/stack/2947/test1.cpp
@@ -0,0 +1,40 @@
+/**
+ * https://www.luogu.com.cn/problem/P2947
+ * 
+ * 单调栈
+ */
+#include <bits/stdc++.h>
+
+const int N = 1e6 + 5;
+int h[N], ans[N];
+std::stack<int> st;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; ++i) {
+        scanf("%d", &h[i]);
+    }
+
+    for(int i = n; i >= 1; --i) {
+        // 比我矮，不是我的仰望对象
+        while(!st.empty() && h[st.top()] <= h[i]) {
+            st.pop();
+        }
+
+        if(st.empty()) {
+            // 没有仰望对象
+            ans[i] = 0;
+        } else {
+            // 有
+            ans[i] = st.top();
+        }
+
+        st.push(i);
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        printf("%d\n", ans[i]);
+    }
+}
