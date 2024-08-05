800d6c96f3284df45f1ef3e744518c93d2506908
Ricky <w_ziwei2004@163.com>
2024-08-05 12:08:19 +0800

    1440

--- luogu/data-structure/queue/1440/test1.cpp
+++ luogu/data-structure/queue/1440/test1.cpp
@@ -1,19 +1,19 @@
 /**
  * https://www.luogu.com.cn/problem/P1440
+ * 
+ * 这题用cin、cout洛谷会超时
  */
 #include <bits/stdc++.h>
 
-const int N = 1e5 + 3;
 std::deque<std::pair<int, int>> dq;
-int s[N];
 
 int main() {
     int n, m;
-    std::cin >> n >> m;
+    scanf("%d %d", &n, &m);
 
     for(int i = 1; i <= n; ++i) {
         int x;
-        std::cin >> x;
+        scanf("%d", &x);
 
         // 出了窗口的弹出
         if(!dq.empty() && dq.front().first < i - m) {
@@ -22,9 +22,9 @@ int main() {
 
         // 此时队头就是最小值
         if(!dq.empty()) {
-            std::cout << dq.front().second << std::endl;
+            printf("%d\n", dq.front().second);
         } else {
-            std::cout << 0 << std::endl;
+            puts("0");
         }
 
         // 把队列尾大于等于x的数弹出，因为它们不会是将来数前m个数中的最小值
