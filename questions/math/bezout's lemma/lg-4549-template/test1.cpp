8b135da93c27ba84d36812b616825274c053a35f
Ricky <w_ziwei2004@163.com>
2024-08-31 20:39:23 +0800

    gcd & lcm

---
+++ questions/math/bezout's lemma/lg-4549-template/test1.cpp
@@ -0,0 +1,5 @@
+/**
+ * @see https://www.luogu.com.cn/problem/P4549
+ */
+#include <bits/stdc++.h>
+
---
+++ questions/math/gcd&lcm/template/test1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+
+/**
+ * 求最大公约数
+ * @par a 第一个数
+ * @par b 第一个数
+ * @return 计算二者的最大公约数
+ */
+ll gcd(ll a, ll b) {
+    return b ? gcd(b, a % b) : a;
+}
+
+/**
+ * 求最小公倍数
+ * @par a 第一个数
+ * @par b 第一个数
+ * @return 计算二者的最小公倍数
+ */
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+
+int main() {
+    ll a = 34;
+    ll b = 85;
+
+    std::cout << gcd(a, b) << std::endl;
+    std::cout << lcm(a, b) << std::endl;
+}
