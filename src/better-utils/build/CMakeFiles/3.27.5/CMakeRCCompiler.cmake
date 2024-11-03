5e49ff4e33f4da7ce55387fd76c9e526ed48503e
Ricky <w_ziwei2004@163.com>
2024-11-03 17:58:14 +0800

    update gitignore

--- src/better-utils/build/CMakeFiles/3.27.5/CMakeRCCompiler.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_RC_COMPILER "F:/application/develop/gcc/x86_64-13.2.0-release-win32-seh-msvcrt-rt_v11-rev0/mingw64/bin/windres.exe")
-set(CMAKE_RC_COMPILER_ARG1 "")
-set(CMAKE_RC_COMPILER_LOADED 1)
-set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
-set(CMAKE_RC_OUTPUT_EXTENSION .obj)
-set(CMAKE_RC_COMPILER_ENV_VAR "RC")
